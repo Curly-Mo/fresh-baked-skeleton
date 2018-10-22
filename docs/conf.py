0238e4fe650cd1553e59a66a5317b590b7d81edc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-22 22:18:48 +0000

    Deploy Curly-Mo/fresh-baked-skeleton to github.com/Curly-Mo/fresh-baked-skeleton.git:master

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ project_slug = 'fresh_baked_skeleton'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.0-alpha.0'
 
 
 # -- General configuration ---------------------------------------------------
